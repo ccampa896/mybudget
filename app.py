af9d8b4ae7cdf2f7cdbb4adbf709af214e6747dc
Carlos Felipe Dalan Campanari <carloskampa@hotmail.com>
2024-01-07 16:35:56 -0300

    v 1.1

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import dash
 import dash_bootstrap_components as dbc
 import dash_auth
+import os
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Isso carrega as variáveis de ambiente do arquivo .env
+
+credenciais_str = os.environ.get('CREDENCIAIS', '{}')
+VALID_USERNAME_PASSWORD_PAIRS = json.loads(credenciais_str)
+
 
 estilos = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "https://fonts.googleapis.com/icon?family=Material+Icons", dbc.themes.COSMO]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
 # FONT_AWESOME = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
 
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'felipe': '1234'
-}
+
 
 app = dash.Dash(__name__, external_stylesheets=estilos + [dbc_css])
 app.title = "Flor de Mel"
