464898c349617055aaad6bc0c8a1ac62e22ef271
Carlos Felipe Dalan Campanari <carloskampa@hotmail.com>
2024-01-09 20:54:45 -0300

    Correção de bugs

--- conexao_banco.py
+++ conexao_banco.py
@@ -6,21 +6,28 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Isso carrega as variáveis de ambiente do arquivo .env
 
-# Obtendo as credenciais do banco de dados a partir das variáveis de ambiente
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
+# # Obtendo as credenciais do banco de dados a partir das variáveis de ambiente
+# DB_HOST = os.environ.get('DB_HOST')
+# DB_PORT = os.environ.get('DB_PORT')
+# DB_NAME = os.environ.get('DB_NAME')
+# DB_USER = os.environ.get('DB_USER')
+# DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
-# Conecta ao banco de dados
-conn = psycopg2.connect(
-    dbname=DB_NAME, 
-    user=DB_USER, 
-    password=DB_PASSWORD, 
-    host=DB_HOST, 
-    port=DB_PORT
-)
+# # Conecta ao banco de dados
+# conn = psycopg2.connect(
+#     dbname=DB_NAME, 
+#     user=DB_USER, 
+#     password=DB_PASSWORD, 
+#     host=DB_HOST, 
+#     port=DB_PORT
+# )
+
+
+# Obtendo a string de conexão do banco de dados a partir das variáveis de ambiente
+DATABASE_URL = os.environ.get('DATABASE_URL')  # Adicione a variável de ambiente correspondente
+
+# Conecta ao banco de dados usando a string de conexão completa
+conn = psycopg2.connect(DATABASE_URL)
 
 # Função para conectar ao banco de dados e ler os dados
 def read_data(query, conn):
