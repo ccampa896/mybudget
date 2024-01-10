13e2d2d4cb527cb2e9eb3d01a97c99b92398428a
Carlos Felipe Dalan Campanari <carloskampa@hotmail.com>
2024-01-10 12:06:54 -0300

    Correção de bugs

--- conexao_banco.py
+++ conexao_banco.py
@@ -31,38 +31,14 @@ conn = psycopg2.connect(DATABASE_URL)
 
 # Função para conectar ao banco de dados e ler os dados
 def read_data(query, conn):
-    try:
-        with conn.cursor() as cur:
-            cur.execute(query)
-            rows = cur.fetchall()
-            columns = [desc[0] for desc in cur.description]
-            return pd.DataFrame(rows, columns=columns)
-    except psycopg2.InterfaceError as e:
-        # Reconectar e tentar novamente
-        conn = psycopg2.connect(
-            dbname=DB_NAME, 
-            user=DB_USER, 
-            password=DB_PASSWORD, 
-            host=DB_HOST, 
-            port=DB_PORT
-        )
-        with conn.cursor() as cur:
-            cur.execute(query)
-            rows = cur.fetchall()
-            columns = [desc[0] for desc in cur.description]
-            return pd.DataFrame(rows, columns=columns)
+    with conn.cursor() as cur:
+        cur.execute(query)
+        rows = cur.fetchall()
+        columns = [desc[0] for desc in cur.description]
+        return pd.DataFrame(rows, columns=columns)
 
 
 def inserir_receita(categoria, data, valor, descricao, fixa, conn):
-    if conn.closed:
-        # Reconectar; substitua as variáveis de ambiente pelas suas credenciais reais
-        conn = psycopg2.connect(
-            dbname=os.environ.get('DB_NAME'), 
-            user=os.environ.get('DB_USER'), 
-            password=os.environ.get('DB_PASSWORD'), 
-            host=os.environ.get('DB_HOST'), 
-            port=os.environ.get('DB_PORT')
-        )
     with conn.cursor() as cur:
         cur.execute(
             'INSERT INTO receitas ("Categoria", "Data", "Valor", "Descrição", "Fixo") VALUES (%s, %s, %s, %s, %s)',
@@ -72,15 +48,6 @@ def inserir_receita(categoria, data, valor, descricao, fixa, conn):
 
 
 def inserir_despesas(categoria, data, valor, descricao, fixa, conn):
-    if conn.closed:
-        # Reconectar; substitua as variáveis de ambiente pelas suas credenciais reais
-        conn = psycopg2.connect(
-            dbname=os.environ.get('DB_NAME'), 
-            user=os.environ.get('DB_USER'), 
-            password=os.environ.get('DB_PASSWORD'), 
-            host=os.environ.get('DB_HOST'), 
-            port=os.environ.get('DB_PORT')
-        )
     with conn.cursor() as cur:
         cur.execute(
             'INSERT INTO despesas ("Categoria", "Data", "Valor", "Descrição", "Fixo") VALUES (%s, %s, %s, %s, %s)',
@@ -90,15 +57,6 @@ def inserir_despesas(categoria, data, valor, descricao, fixa, conn):
 
 
 def inserir_categoria(nome_categoria, conn, tabela):
-    if conn.closed:
-        # Reconectar; substitua as variáveis de ambiente pelas suas credenciais reais
-        conn = psycopg2.connect(
-            dbname=os.environ.get('DB_NAME'), 
-            user=os.environ.get('DB_USER'), 
-            password=os.environ.get('DB_PASSWORD'), 
-            host=os.environ.get('DB_HOST'), 
-            port=os.environ.get('DB_PORT')
-        )
     with conn.cursor() as cur:
         cur.execute(
             f"INSERT INTO {tabela} (nome_categoria) VALUES (%s)",
@@ -108,15 +66,6 @@ def inserir_categoria(nome_categoria, conn, tabela):
 
 
 def deletar_categoria(nome_categoria, conn, tabela):
-    if conn.closed:
-        # Reconectar; substitua as variáveis de ambiente pelas suas credenciais reais
-        conn = psycopg2.connect(
-            dbname=os.environ.get('DB_NAME'), 
-            user=os.environ.get('DB_USER'), 
-            password=os.environ.get('DB_PASSWORD'), 
-            host=os.environ.get('DB_HOST'), 
-            port=os.environ.get('DB_PORT')
-        )
     with conn.cursor() as cur:
         cur.execute(
             f"DELETE FROM {tabela} WHERE nome_categoria = %s",
--- globals.py
+++ globals.py
@@ -3,21 +3,18 @@ import os
 from app import *
 from conexao_banco import *
 
-try:
-    # Lendo as categorias de receitas e despesas
-    df_cat_receita = read_data("SELECT nome_categoria FROM cat_receitas", conn)
-    df_cat_despesa = read_data("SELECT nome_categoria FROM cat_despesas", conn)
-    cat_receita = df_cat_receita['nome_categoria'].tolist()
-    cat_despesa = df_cat_despesa['nome_categoria'].tolist()
 
-    # Lendo as tabelas de despesas e receitas
-    df_despesas = read_data("SELECT * FROM despesas", conn)
-    df_receitas = read_data("SELECT * FROM receitas", conn)
+# Lendo as categorias de receitas e despesas
+df_cat_receita = read_data("SELECT nome_categoria FROM cat_receitas", conn)
+df_cat_despesa = read_data("SELECT nome_categoria FROM cat_despesas", conn)
+cat_receita = df_cat_receita['nome_categoria'].tolist()
+cat_despesa = df_cat_despesa['nome_categoria'].tolist()
 
-    # Convertendo a coluna 'Data' para o formato de data
-    df_despesas["Data"] = pd.to_datetime(df_despesas["Data"]).dt.date
-    df_receitas["Data"] = pd.to_datetime(df_receitas["Data"]).dt.date
+# Lendo as tabelas de despesas e receitas
+df_despesas = read_data("SELECT * FROM despesas", conn)
+df_receitas = read_data("SELECT * FROM receitas", conn)
 
-finally:
-    # Fecha a conexão
-    conn.close()
+# Convertendo a coluna 'Data' para o formato de data
+df_despesas["Data"] = pd.to_datetime(df_despesas["Data"]).dt.date
+df_receitas["Data"] = pd.to_datetime(df_receitas["Data"]).dt.date
+
